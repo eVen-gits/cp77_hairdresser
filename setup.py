cd323631677ce229b0ad3227697b23e477c6b1fd
even-gits <egej.vencelj@gmail.com>
2020-12-22 23:34:43 +0100

    *Update: PIP packaging

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name='cp77_hairdresser',
-    version='0.0.1',    
-    description='TODO',
-    url='git@github.com:eVen-gits/cp77_hairdresser.git',
-    author='Egej Vencelj',
-    author_email='egej.vencelj@gmail.com',
-    license='unlicense',
-    packages=['cp77_hairdresser'],
-    zip_safe=False
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="even-gits",
+    version="0.0.1",
+    author="Egej Vencelj",
+    author_email="egej.vencelj@gmail.com",
+    description="Cyberpunk 2077 hairstyle changer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/eVen-gits/cp77_hairdresser",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
