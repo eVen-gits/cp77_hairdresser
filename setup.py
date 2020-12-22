3d6d2634eec1bc5bc329716982a6e2e7d5709441
even-gits <egej.vencelj@gmail.com>
2020-12-22 23:18:05 +0100

    *Update: Initial update

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='cp77_hairdresser',
+    version='0.0.1',    
+    description='TODO',
+    url='git@github.com:eVen-gits/cp77_hairdresser.git',
+    author='Egej Vencelj',
+    author_email='egej.vencelj@gmail.com',
+    license='unlicense',
+    packages=['cp77_hairdresser'],
+    zip_safe=False
+)
